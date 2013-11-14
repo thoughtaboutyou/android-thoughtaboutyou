cf61f4a1cfa40c3a79bd32806a7d88d091e796e7
Joel Purra <code+github@joelpurra.com>
2013-11-14 09:32:43 +0100

    Add RoboGuice as a Maven dependency
    Inherit from RoboActivity

--- project/src/net/thoughtaboutyou/app/android/MainActivity.java
+++ project/src/net/thoughtaboutyou/app/android/MainActivity.java
@@ -1,10 +1,10 @@
 package net.thoughtaboutyou.app.android;
 
-import android.app.Activity;
+import roboguice.activity.RoboActivity;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends RoboActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
