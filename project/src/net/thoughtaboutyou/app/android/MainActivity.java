31c260a8208adbecbdae1d92e3845fc8122567a6
Joel Purra <code+github@joelpurra.com>
2013-11-19 19:45:51 +0100

    Add a dummy button to call someone

--- project/src/net/thoughtaboutyou/app/android/MainActivity.java
+++ project/src/net/thoughtaboutyou/app/android/MainActivity.java
@@ -1,15 +1,27 @@
 package net.thoughtaboutyou.app.android;
 
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends RoboActivity {
+	@InjectView(R.id.thinkAboutSomeoneButton)
+	Button thinkAboutSomeoneButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		thinkAboutSomeoneButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				// TODO: call someone
+			}
+		});
 	}
 
 	@Override
