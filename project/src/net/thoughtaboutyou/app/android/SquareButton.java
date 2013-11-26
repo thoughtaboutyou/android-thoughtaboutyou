c250fa0f864335cfecd4c98f276e0756e6d5ac9a
Joel Purra <code+github@joelpurra.com>
2013-11-26 14:10:28 +0100

    Make button square on all devices, increaste text size, make it bold

---
+++ project/src/net/thoughtaboutyou/app/android/SquareButton.java
@@ -0,0 +1,26 @@
+package net.thoughtaboutyou.app.android;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class SquareButton extends Button {
+	public SquareButton(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public SquareButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public SquareButton(Context context) {
+		super(context);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// http://stackoverflow.com/questions/4656986/how-do-i-keep-the-aspect-ratio-on-image-buttons-in-android
+		// http://stackoverflow.com/a/7979110/
+		super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+	}
+}
